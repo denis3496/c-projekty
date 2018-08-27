d5d52b6ebca92305a290925168f83d541a88032f
Dominic Denis <deniszebrowski@gmail.com>
2018-08-27 23:48:21 +0200

    Signed-off-by: dman <deniszebrowski@gmail.com>

--- workspace_oxygen/smarowniae/main.c
+++ workspace_oxygen/smarowniae/main.c
@@ -14,7 +14,9 @@
 #include <util/delay.h>
 #include <stdint.h>
 #include <avr/interrupt.h>
-
+#include <avr/pgmspace.h>
+#include <string.h>
+#include "lcd44780.h"
 
 #define DIP1 (1<<PD0)
 #define DIP2 (1<<PD1)
@@ -29,57 +31,57 @@
 #define T_TEST (1<<PB2)
 #define T_POZIOM (1<<PB1)
 //wszystkie io- wejscia zanegowac!!!
-#define BTN_DEBOUCE 20 //ms
-
-ISR(INT0_vect) { //TYLKO NA PINIE PD2!!!
-
-    uint16_t timer = 0;
-    while(bit_is_set(PIND, PD2)) { // button hold down- WYKRYWA STAN NISKI- GDY GUZIK ZWIERA DO MASY
-        timer++; // count how long button is pressed
-        _delay_ms(1);
-    }
-    if(timer > BTN_DEBOUCE) { // software debouncing button
-        if(timer < 5000UL) {//unsigned long
-            //single click
-        	  PORTD &= ~(1<<PD5); //Turns OFF LED
-        	      _delay_ms(500); //3 second delay
-        	      PORTD |= (1<<PD5); //Turns ON LED
-
-        } else {
-            //button hold
-
-        					PORTD &= ~(1<<PD5); //Turns OFF LED
-        	        	      _delay_ms(3000); //3 second delay
-        	        	      PORTD |= (1<<PD5); //Turns ON LED- LEDY PODLACZONE DO VCC- SWIECI CALY CZAS
-        	        	      _delay_ms(3000); //3 second delay
-        	        	      PORTD &= ~(1<<PD5); //Turns OFF LED
-        	        	      _delay_ms(3000); //3 second delay
-        	        	      PORTD |= (1<<PD5);
-        }
-    }
-    timer=0;
-}
-
+//--------------------------------------
+#define IDLE 0
+#define ACTIVE 50 // odpowiada wypelnieniu PWM
+volatile unsigned char MOTOR_STAN;
+//-----------------------------------------
+#define czas_pracy 10 //ile sek krecic, kreci 10 sek i wylacza
+volatile uint16_t motor_wait;//co ile wlaczac silnik
+volatile uint16_t sek_count;// volatile mowi ze zmienna nalezy do wielu watkow dlatego nie mozna jej optymalizowac
+//------------------------------------------
 void io_init()
 {
-	  DDRD = 0b11000000;
+	//  DDRD = 0b11000000;
 	  //WEJ NA DIP Z REZYSTORAMI PULL DOWN, PD4-WEJ, PD5- WEJ KONTROLKA,PD6-WYJ NA CEWKE, PD7-WY NA BTS
+	//tymczasowo:
+	  DDRD = 0b11111011; //PD2 GUZIK
+	  DDRD &= ~(1<<PD2);//Makes firs pin of PORTD as Input
+	  PORTD |= (1<<PD2);
+
 	  DDRB = 0b00111001;
-	  // PB0- WYJ NA BUZZ, PB1(T_POZIOM), PB2(T_TEST)- WEJSCIA Z TRANSOPTORA, PB3,PB4,PB5-MAGISTRALA SPI
+	  // PB0- WYJ NA BUZZ, PB1(T_POZIOM), PB2(T_TEST)- WEJSCIA Z TRANSOPTORA, PB3(OC2-pwm),PB4,PB5-MAGISTRALA SPI
+
+}
+
+void initInterrupt2(void) {// TIMER1 INIT
+	TCCR1A |= 0;
+	TCCR1B |= (1 << WGM12)|(1<<CS12) | (1<<CS10); // Ÿród³em CLK, preskaler 64, Mode = CTC,
+	// enable compare interrupt
+		TIMSK |= (1 << OCIE1A);
+	TCNT1 = 0;
+	// initialize compare value
+		OCR1A = 8333; // ok. 1SEK
+
+		sek_count=0;
 
 }
 
 void initInterrupt1(void)
 {
-	//TCCR0 |=(1<<WGM00)|(1<<WGM01)|(1<<COM00)|(1<<COM01)|(1<<CS00);
-	 //   OCR0 = 0;
+
+	    TCCR0=0b01110101; //Configure TCCR0 as explained in the article
+	    OCR0=0; // Set OCR0 to 0 so that the duty cycle is initially 0 and the motor is not rotating, 127- plynnie jedzie
+	   /*	TCCR0= [FOC0	|WGM00|	COM01|	COM00|	WGM01|	CS02|	CS01|	CS00]
+	    Set bits WGM00 and WGM01 to 1 and 0 respectively. This enables the phase correct PWM mode.
+	    Set bits COM00 and COM01 to 0 and 1 respectively. This means that the generated PWM will be an inverted PWM.
+	    Set bits CS00 and CS01 and CS02 to 1, 0 and 1 respectively. This means that the counter will be clocked from the system clock divided by 1024.*/
 }
 
 void initInterrupt0(void) {
-    GIMSK |= (1 << INT0); // general interrupt mask register -> enable interrupt on INT0
+    GICR |= (1 << INT0); // general interrupt mask register -> enable interrupt on INT0. dla atmegi 8- GIMSK
     MCUCR &= ~(1 << ISC00); // trigger interrupt on falling edge
     MCUCR |= (1 << ISC01); // trigger interrupt on falling edge
-    sei(); //  set interrupt enable bit
 }
 
 uint8_t time_switch()
@@ -87,23 +89,82 @@ uint8_t time_switch()
 	unsigned char io = PIND;
 	uint8_t tmp;
 
-	if((io & DIP1)==0) tmp=10; // GDY ZALACZONY POSZCZEGOLNY SWITCH
+/*	if((io & DIP1)==0) tmp=10; // GDY ZALACZONY POSZCZEGOLNY SWITCH. trzeba zresetowac po zmianie
 	if((io & DIP2)==0) tmp=20;
 	if((io & DIP3)==0) tmp=30;
-	if((io & DIP4)==0) tmp=40;
+	if((io & DIP4)==0) tmp=40;*/
 
 	return tmp;
 }
 
+void greeting(){
+	char napis1[] = "HELLO";
+	lcd_cls();
+		lcd_locate(1,1);
+		lcd_str(napis1);
+		_delay_ms(1500);
+		lcd_cls();
+}
 
+ISR(INT0_vect) { //TYLKO NA PINIE PD2!!!
+	lcd_cls();
+    uint16_t timer = 0;
+    while(bit_is_clear(PIND, PD2)) { // button hold down- WYKRYWA STAN NISKI- GDY GUZIK ZWIERA DO MASY
+        timer++; // count how long button is pressed
+        lcd_locate(1,1);
+        lcd_int(timer);
+           _delay_ms(500);
+    }
+    	if(timer !=0) motor_wait = timer;
+
+}
+ISR(TIMER1_COMPA_vect) //tik co ok. 1 sekunde
+{
+	   sek_count++; // zwieksz globalna ilosc sekund
+
+	   lcd_locate(0,1);
+	   lcd_int(motor_wait);
+	   lcd_locate(0,6);
+	   lcd_int(sek_count);
+	   _delay_ms(500);
+	   lcd_cls();
+	   //tu sprawdzanie globalnej flagi sekund
+
+	   if(sek_count==czas_pracy && MOTOR_STAN==ACTIVE)  {
+		   MOTOR_STAN=IDLE;
+		   OCR0 =0; // WLACZA SILNIK LUB WYLACZA CO 3 SEKUNDY NA PRZEMIAN
+		   sek_count =0; //zliczaj od nowa sekundy
+	   }
+	   else if(sek_count==motor_wait && MOTOR_STAN==IDLE){
+		   MOTOR_STAN=ACTIVE;
+		   OCR0 = 50;
+		   sek_count =0; //zliczaj od nowa sekundy
+	   }
+
+
+
+
+
+}
 int main(void)
 {
 	uint8_t TIME1, TIME2;
+	  DDRD |= (1<<PD1); //Nakes first pin of PORTC as Output
+	  PORTD |= (1<<PD1); //WYLACZA LED BO DO LED DO VCC. GDYBY LED BYL DO MASY TO TA KOMENDA WLACZA LED
 	io_init();
+	lcd_init();
+
+
+	greeting();
 	initInterrupt1();//pwm
 	initInterrupt0();//test guzik
+	initInterrupt2();//pokazuje czas co 3 sekundy
+
+
 	 TIME1= time_switch();
-	 TIME2= test_butt();
+	// TIME2= test_butt();
+
+	sei();
 
 	 while(1);
 
